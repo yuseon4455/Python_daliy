34acc2e1494812337e662152786797f807fe1fd0
dimsum <105402328+yuseon4455@users.noreply.github.com>
2025-09-29 15:36:20 +0000

    폴더 추가 후 변경

---
+++ day1/main.py
@@ -0,0 +1,39 @@
+print("Hello, World!")
+
+# 변수에 숫자 저장
+a = 2
+b = 3
+c = a + b
+print(c) 
+# 5
+
+a = 2
+b = 3
+c = a + b
+c = 1
+print(c)
+# 1
+
+a = 2
+b = 3
+c = a + b
+a = 1
+b = 10
+print(c)
+# 5
+
+# 데이터 타입(숫자)
+my_age = 88
+
+# 데이터 타입(문자)
+my_name = "Python"
+print(my_name)
+
+# boolean
+# "", 소문자 X
+dead = False
+print(dead)
+
+# 마무리
+print("Hello my name is",my_name)
+print("and I'm ", my_age," years old")
